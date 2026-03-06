c3de660fca62e91d05aec177599fa31ec4577d5d
zonde306 <anonymous@localhost>
2026-03-07 04:19:42 +0800

    fix: missing file

---
+++ src/utils/events.ts
@@ -0,0 +1,8 @@
+
+export const eventTypes = {
+    GENERATION_STREAM_CHUNK: 'cg_generate_chunk',
+    GENERATION_DONE: 'cg_generate_done',
+    MESSAGE_CREATED: 'cg_message_created',
+    RECORD_UPDATING: 'cg_record_updating',
+    RECORD_UPDATED: 'cg_record_updated',
+}
